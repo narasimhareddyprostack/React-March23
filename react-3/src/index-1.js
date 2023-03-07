27fce09964317c4df0a856ae40dbda9c0a51fc01
narasimhareddyprostack <narasimhareddyk18@gmail.com>
2023-03-07 18:23:47 +0530

    test

---
+++ react-3/src/index-1.js
@@ -0,0 +1,4 @@
+import ReactDOM from 'react-dom'
+//ReactDOM.render(1,2)
+//ReactDOM.render(what you want render,where you want render)
+ReactDOM.render("Hello,GA", document.getElementById('nayana'))
---
+++ react-3/src/index-2.js
@@ -0,0 +1,2 @@
+import ReactDOM from 'react-dom'
+ReactDOM.render(<div><h1>Hello,GA</h1><h2>Lunch time</h2></div>, document.getElementById('nayana'))
